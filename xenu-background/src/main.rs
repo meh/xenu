9204e62e38d06b837def806970f52a90b270cec6
meh <meh@schizofreni.co>
2016-09-09 22:31:23 +0200

    background: handle atoms and resources

--- xenu-background/src/main.rs
+++ xenu-background/src/main.rs
@@ -24,6 +24,12 @@ use image::GenericImage;
 extern crate xcb;
 extern crate xcb_util as xcbu;
 
+const ATOMS: &'static [&'static str] = &[
+	"_XROOTPMAP_ID",      // hsetroot
+	"_XSETROOT_ID",       // xsetroot
+	"_XENU_BACKGROUND_ID" // meh
+];
+
 fn main() {
 	let matches = App::new("xenu-background")
 		.version(env!("CARGO_PKG_VERSION"))
@@ -45,13 +51,25 @@ fn main() {
 	let setup  = connection.get_setup();
 	let screen = setup.roots().nth(screen as usize).unwrap();
 
+	// Open the given path and resize it to fit the screen.
 	let source = image::open(path).expect("failed to open image")
 		.resize(screen.width_in_pixels() as u32, screen.height_in_pixels() as u32,
 			image::FilterType::Lanczos3);
 
-	let mut image = xcbu::image::shm::create(&connection, screen.root_depth(),
-		screen.width_in_pixels(), screen.height_in_pixels()).unwrap();
+	clean(&connection, &screen);
+	set(&connection, &screen, source.to_rgb());
 
+	connection.flush();
+}
+
+/// Set the background for the screen from the given image.
+fn set<T: GenericImage<Pixel = image::Rgb<u8>>>(c: &xcb::Connection, screen: &xcb::Screen, source: T) {
+	// Create a shared image to store the pixels.
+	let mut image = xcbu::image::shm::create(c, screen.root_depth(),
+		screen.width_in_pixels(), screen.height_in_pixels())
+			.expect("could not create image");
+
+	// Fill in the pixels from the source image.
 	for (x, y, px) in source.pixels() {
 		image.put(x, y,
 			((px[0] as u32) << 16) |
@@ -59,26 +77,63 @@ fn main() {
 			((px[2] as u32) << 0));
 	}
 
-	let pixmap = connection.generate_id();
-	xcb::create_pixmap_checked(&connection, screen.root_depth(), pixmap, screen.root(),
+	// Create the pixmap that will store the background.
+	let pixmap = c.generate_id();
+	xcb::create_pixmap_checked(c, screen.root_depth(), pixmap, screen.root(),
 		screen.width_in_pixels(), screen.height_in_pixels())
 			.request_check().expect("could not create pixmap");
 
-	let context = connection.generate_id();
-	xcb::create_gc_checked(&connection, context, screen.root(), &[])
+	// Create the useless graphics context.
+	let context = c.generate_id();
+	xcb::create_gc_checked(c, context, screen.root(), &[])
 		.request_check().expect("could not create context");
 
-	xcbu::image::shm::put(&connection, pixmap, context, &image, 0, 0, 0, 0,
+	// Push the shared image into the pixmap.
+	xcbu::image::shm::put(c, pixmap, context, &image, 0, 0, 0, 0,
 		screen.width_in_pixels(), screen.height_in_pixels(), false)
 			.expect("could not draw image");
 
-	xcb::change_window_attributes_checked(&connection, screen.root(), &[
+	// Set the created pixmap as root background.
+	xcb::change_window_attributes_checked(c, screen.root(), &[
 		(xcb::CW_BACK_PIXMAP, pixmap)])
 			.request_check().expect("could not change attributes");
 
-	xcb::clear_area_checked(&connection, true, screen.root(), 0, 0,
+	// Clear the root window so the background image is refreshed.
+	xcb::clear_area_checked(c, true, screen.root(), 0, 0,
 		screen.width_in_pixels(), screen.height_in_pixels())
 			.request_check().expect("could not clear root window");
 
-	connection.flush();
+	// Set background ID.
+	for atom in ATOMS {
+		xcb::change_property(c, xcb::PROP_MODE_REPLACE as u8, screen.root(),
+			xcb::intern_atom(c, false, atom).get_reply().expect("failed to intern atom").atom(),
+			xcb::ATOM_PIXMAP, 32, &[pixmap]);
+	}
+}
+
+/// Clean up any previously created resources.
+fn clean(c: &xcb::Connection, screen: &xcb::Screen) {
+	// Fetch any previously set pixmap IDs.
+	let ids = ATOMS.iter().map(|atom| {
+		let reply = xcb::get_property(c, false, screen.root(),
+			xcb::intern_atom(c, false, atom).get_reply().expect("failed to intern atom").atom(),
+			xcb::ATOM_PIXMAP, 0, 1).get_reply();
+
+		match reply {
+			Ok(ref reply) if reply.type_() == xcb::ATOM_PIXMAP =>
+				Some(reply.value()[0]),
+
+			_ =>
+				None
+		}
+	}).collect::<Vec<Option<xcb::Pixmap>>>();
+
+	// If all are set and equal, kill it with fire.
+	if ids.iter().all(Option::is_some) && ids.iter().all(|id| id == ids.first().unwrap()) {
+		xcb::kill_client(c, ids.first().unwrap().unwrap());
+	}
+
+	// Kill any temporary resources.
+	xcb::kill_client(c, xcb::KILL_ALL_TEMPORARY);
+	xcb::set_close_down_mode(c, xcb::CLOSE_DOWN_RETAIN_TEMPORARY as u8);
 }
